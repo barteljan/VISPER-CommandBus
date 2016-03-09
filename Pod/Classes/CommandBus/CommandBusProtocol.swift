f7c8ea60c3ca604c575549bf2e3db41ba3bb74e8
Jan Bartel <barteljan@yahoo.de>
2016-03-09 08:09:07 +0100

    Command handler eine convinience methode process ohne generics hinzugefuegt

--- Pod/Classes/CommandBus/CommandBusProtocol.swift
+++ Pod/Classes/CommandBus/CommandBusProtocol.swift
@@ -11,8 +11,11 @@ import Foundation
 public protocol CommandBusProtocol {
     
     func isResponsible(command: Any!) -> Bool
-    func process<ResultType>(command: Any!, completion: ((result: ResultType?, error: ErrorType?) -> Void)?)  throws 
+    
     func process(command: Any!) throws
+    func process(command: Any!, completion: ((result: Any?, error: ErrorType?) -> Void)?) throws
+    func process<T>(command: Any!, completion: ((result: T?, error: ErrorType?) -> Void)?)  throws
+    
 
     
     func addHandler(handler:CommandHandlerProtocol)
@@ -23,8 +26,18 @@ public protocol CommandBusProtocol {
 
 public extension CommandBusProtocol{
 
-    public  func process(command: Any!) throws{
+    public func process(command: Any!) throws{
         try self.process(command) { (myResult : AnyObject?, myError: ErrorType?) -> Void in}
+    }
+    
+    public func process<T>(command: Any!, completion: ((result: T?, error: ErrorType?) -> Void)?)  throws{
+        
+        let myCompletion = completion
+        
+        try self.process(command) { (result, error) -> Void in
+            myCompletion?(result:(result as! T?),error: error)
+        }
+        
     }
 
 }
